c1cbea4e0ef854d95bcff5b4c23d5e4a8d0f8c32
unknown <brunoeiterer@gmail.com>
2019-05-04 15:42:44 -0300

    Add tree node

---
+++ Node.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenealogyTree
+{
+    class Node<T>
+    {
+        public T Value { get; set; }
+        private List<Node<T>> children;
+        private Node<T> Parent { get; set; }
+
+        public Node(T value, Node<T> parent)
+        {
+            Value = value;
+            children = new List<Node<T>>();
+            Parent = parent;
+        }
+
+        public void AddChild(T value, Node<T> parent)
+        {
+            children.Add(new Node<T>(value, parent));
+        }
+
+        public void InsertChild(Node<T> node, int index)
+        {
+            children.Insert(index, node);
+        }
+
+        public void Remove(Node<T> node)
+        {
+            children.Remove(node);
+        }
+    }
+}
