9c5f84f52ffe972c76058d03f072f57ceec4e33b
unknown <brunoeiterer@gmail.com>
2019-05-04 15:29:40 -0300

    Add Person class

---
+++ Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenealogyTree
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Partner { get; set; }
+        public DateTime birthDate { get; set; }
+        public DateTime deathDate { get; set; }
+
+        public Person() : this(string.Empty, string.Empty)
+        {
+
+        }
+
+        public Person(string name) : this(name, string.Empty)
+        {
+
+        }
+
+        public Person(string name, string partner)
+        {
+            this.Name = name;
+            this.Partner = partner;
+        }
+    }
+}
