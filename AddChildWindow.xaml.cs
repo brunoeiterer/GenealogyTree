f166e3df424bb9f15b66ad1a3dedf47cac6a68d6
unknown <brunoeiterer@gmail.com>
2019-05-20 13:28:11 -0300

    Disable single parent child addition

--- AddChildWindow.xaml.cs
+++ AddChildWindow.xaml.cs
@@ -36,98 +36,105 @@ namespace GenealogyTree
                 Node<Person> parent;
                 parent = PersonTree.GetNodeByName(PersonTree.Tree, this.ParentCombobox.SelectedItem.ToString());
 
-                Person newPerson = new Person(this.NewPersonName.Text);
-                string errorMessage = string.Empty;
-
-                DateTime birthDate;
-                DateTime deathDate;
-
-                try
+                if(parent.Value.Partner != string.Empty)
                 {
-                    birthDate = DateTime.ParseExact(this.NewPersonBirthDate.Text.ToString(), "dd/MM/yyyy", null, DateTimeStyles.None);
-                    newPerson.BirthDate = (Nullable<DateTime>) birthDate;
-                }
-                catch(FormatException)
-                {
-                    if(this.NewPersonBirthDate.Text.ToString() == string.Empty)
-                    {
-                        newPerson.BirthDate = null;
-                    }
-                    else
-                    {
-                        errorMessage += (string)FindResource("AddChildWindowBirthDateFormatError");
-                    }
-                }
+                    Person newPerson = new Person(this.NewPersonName.Text);
+                    string errorMessage = string.Empty;
 
-                try
-                {
-                    deathDate = DateTime.ParseExact(this.NewPersonDeathDate.Text.ToString(), "dd/MM/yyyy", null, DateTimeStyles.None);
-                    newPerson.DeathDate = (Nullable<DateTime>)deathDate;
-                }
-                catch (FormatException)
-                {
-                    if (this.NewPersonDeathDate.Text.ToString() == string.Empty)
-                    {
-                        newPerson.BirthDate = null;
-                    }
-                    else
-                    {
-                        errorMessage += "\n" + (string)FindResource("AddChildWindowDeathDateFormatError");
-                    }
-                }
-
-                if (this.NewPersonPartnerName.Text.ToString() != string.Empty)
-                {
-                    newPerson.Partner = this.NewPersonPartnerName.Text;
-
-                    DateTime partnerBirthDate;
-                    DateTime partnerDeathDate;
+                    DateTime birthDate;
+                    DateTime deathDate;
 
                     try
                     {
-                        partnerBirthDate = DateTime.ParseExact(this.NewPersonPartnerBirthDate.Text.ToString(), "dd/MM/yyyy", null, DateTimeStyles.None);
-                        newPerson.PartnerBirthDate = (Nullable<DateTime>)partnerBirthDate;
+                        birthDate = DateTime.ParseExact(this.NewPersonBirthDate.Text.ToString(), "dd/MM/yyyy", null, DateTimeStyles.None);
+                        newPerson.BirthDate = (Nullable<DateTime>)birthDate;
                     }
                     catch (FormatException)
                     {
-                        if (this.NewPersonPartnerBirthDate.Text.ToString() == string.Empty)
+                        if (this.NewPersonBirthDate.Text.ToString() == string.Empty)
                         {
-                            newPerson.PartnerBirthDate = null;
+                            newPerson.BirthDate = null;
                         }
                         else
                         {
-                            errorMessage += "\n" + (string)FindResource("AddChildWindowPartnerBirthDateFormatError");
+                            errorMessage += (string)FindResource("AddChildWindowBirthDateFormatError");
                         }
                     }
+
                     try
                     {
-                        partnerDeathDate = DateTime.ParseExact(this.NewPersonPartnerDeathDate.Text.ToString(), "dd/MM/yyyy", null, DateTimeStyles.None);
-                        newPerson.PartnerDeathDate = (Nullable<DateTime>)partnerDeathDate;
+                        deathDate = DateTime.ParseExact(this.NewPersonDeathDate.Text.ToString(), "dd/MM/yyyy", null, DateTimeStyles.None);
+                        newPerson.DeathDate = (Nullable<DateTime>)deathDate;
                     }
                     catch (FormatException)
                     {
-                        if (this.NewPersonPartnerDeathDate.Text.ToString() == string.Empty)
+                        if (this.NewPersonDeathDate.Text.ToString() == string.Empty)
                         {
-                            newPerson.PartnerDeathDate = null;
+                            newPerson.BirthDate = null;
                         }
                         else
                         {
-                            errorMessage += "\n" + (string)FindResource("AddChildWindowPartnerDeathDateFormatError");
+                            errorMessage += "\n" + (string)FindResource("AddChildWindowDeathDateFormatError");
                         }
                     }
-                }
 
-                if (errorMessage == string.Empty)
-                {
-                    parent.AddChild(newPerson, parent);
-                    parent.Children[parent.Children.Count - 1].SubscribeToNewChildAdded(PersonTree.NewChildAdded);
-                    this.Close();
+                    if (this.NewPersonPartnerName.Text.ToString() != string.Empty)
+                    {
+                        newPerson.Partner = this.NewPersonPartnerName.Text;
+
+                        DateTime partnerBirthDate;
+                        DateTime partnerDeathDate;
+
+                        try
+                        {
+                            partnerBirthDate = DateTime.ParseExact(this.NewPersonPartnerBirthDate.Text.ToString(), "dd/MM/yyyy", null, DateTimeStyles.None);
+                            newPerson.PartnerBirthDate = (Nullable<DateTime>)partnerBirthDate;
+                        }
+                        catch (FormatException)
+                        {
+                            if (this.NewPersonPartnerBirthDate.Text.ToString() == string.Empty)
+                            {
+                                newPerson.PartnerBirthDate = null;
+                            }
+                            else
+                            {
+                                errorMessage += "\n" + (string)FindResource("AddChildWindowPartnerBirthDateFormatError");
+                            }
+                        }
+                        try
+                        {
+                            partnerDeathDate = DateTime.ParseExact(this.NewPersonPartnerDeathDate.Text.ToString(), "dd/MM/yyyy", null, DateTimeStyles.None);
+                            newPerson.PartnerDeathDate = (Nullable<DateTime>)partnerDeathDate;
+                        }
+                        catch (FormatException)
+                        {
+                            if (this.NewPersonPartnerDeathDate.Text.ToString() == string.Empty)
+                            {
+                                newPerson.PartnerDeathDate = null;
+                            }
+                            else
+                            {
+                                errorMessage += "\n" + (string)FindResource("AddChildWindowPartnerDeathDateFormatError");
+                            }
+                        }
+                    }
+
+                    if (errorMessage == string.Empty)
+                    {
+                        parent.AddChild(newPerson, parent);
+                        parent.Children[parent.Children.Count - 1].SubscribeToNewChildAdded(PersonTree.NewChildAdded);
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show(errorMessage, (string)FindResource("AddChildWindowDateFormatErrorMessageBoxTitle"));
+                    }
                 }
                 else
                 {
-                    MessageBox.Show(errorMessage, (string)FindResource("AddChildWindowDateFormatErrorMessageBoxTitle"));
+                    MessageBox.Show((string)FindResource("AddChildWindowEmptyParentPartnerError"),
+                        (string)FindResource("AddChildWindowEmptyParentPartnerErrorMessageBoxTitle"));
                 }
-
             }
         }
 
